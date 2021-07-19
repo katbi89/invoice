b6ef97c8503bd77f09fb502b7e17f646930835c6
katbi89 <katbi89@gmail.com>
2021-07-19 21:49:03 +0300

    invoice not complete

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('12345678'),
+            'roles_name' => ["owner"],
+            'status' => 'مفعل',
+        ]);
+
+        $role = Role::create(['name' => 'owner']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
